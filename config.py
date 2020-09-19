445eb6fce166a2ac0127ac61ec4546924cfad940
Joe Ipson <joe@ipson.me>
2020-09-18 21:44:51 -0600

    Prevent signups with env var

--- config.py
+++ config.py
@@ -6,6 +6,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
   JWT_SECRET_KEY = os.environ.get('API_SECRET_KEY')
   DB_ENCRYPTION_KEY = os.environ.get('DB_ENCRYPTION_KEY')
+  PREVENT_SIGNUPS = os.environ.get('PREVENT_SIGNUPS', False)
   SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or 'sqlite:///' + os.path.join(basedir + '/config', 'app.db')
   SQLALCHEMY_TRACK_MODIFICATIONS = False
   JWT_ACCESS_TOKEN_EXPIRES = datetime.timedelta(days=7)
